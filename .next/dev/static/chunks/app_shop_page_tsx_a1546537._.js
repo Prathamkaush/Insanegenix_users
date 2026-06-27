39f9fed57a36991d1a33cb0ace0ff5b52f5e507e
Prathamkaush <prathamkaushik2002@gmail.com>
2026-06-27 17:13:57 +0530

    stlyiing fixes

---
+++ .next/dev/static/chunks/app_shop_page_tsx_a1546537._.js
@@ -0,0 +1,8 @@
+(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/_97d67bbc._.js",
+  "static/chunks/components_d6b8842c._.css"
+],
+    source: "dynamic"
+});
